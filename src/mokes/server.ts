55d5b1bacdd31c7716442a2f76af74c55a1f6447
지인혁 <dhgg321@naver.com>
2024-02-07 17:31:28 +0900

    Feat: msw 서버 인터셉터 확인 기능 추가

--- src/mokes/server.ts
+++ src/mokes/server.ts
@@ -2,3 +2,7 @@ import { setupServer } from 'msw/node';
 import { handler } from './handler';
 
 export const server = setupServer(...handler);
+
+server.events.on('request:start', ({ request }) => {
+  console.log('MSW intercepted:', request.method, request.url);
+});
