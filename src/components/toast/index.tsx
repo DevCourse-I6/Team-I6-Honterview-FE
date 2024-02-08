7dd955e771a1f762f433c301e9720c2b11ac0852
지인혁 <dhgg321@naver.com>
2024-02-08 10:56:07 +0900

    Feat: Toast 컴포넌트 개발

---
+++ src/components/toast/index.tsx
@@ -0,0 +1,59 @@
+import {
+  toast,
+  Bounce,
+  ToastContainer as ToastOption,
+  ToastOptions,
+} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { TAlert } from './types';
+
+const ToastContainer = () => {
+  return (
+    <ToastOption
+      position="top-right"
+      autoClose={1000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce}
+    />
+  );
+};
+
+const Toast = ({ message }: { message: string }) => {
+  return (
+    <article>
+      <h3 className="text-base">{message}</h3>
+    </article>
+  );
+};
+
+export const notify = (
+  type: TAlert,
+  message: string,
+  options?: ToastOptions,
+) => {
+  switch (type) {
+    case 'error':
+      toast.error(<Toast message={message} />, options && options);
+      break;
+    case 'success':
+      toast.success(<Toast message={message} />, options && options);
+      break;
+    case 'info':
+      toast.info(<Toast message={message} />, options && options);
+      break;
+    case 'warning':
+      toast.warning(<Toast message={message} />, options && options);
+      break;
+    default:
+      break;
+  }
+};
+
+export default ToastContainer;
