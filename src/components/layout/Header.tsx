484d0d13da8759ce3f7a7d5d9e7248b383f4d6e5
nimgnas <sangminyn@gmail.com>
2024-02-14 02:23:59 +0900

    Feat: Header 컴포넌트

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+
+const navigation = [
+  {
+    title: '면접질문',
+    url: '/',
+    replace: false,
+    scroll: false,
+  },
+  {
+    title: '면접연습',
+    url: '/',
+    replace: false,
+    scroll: false,
+  },
+];
+
+const Header = () => {
+  return (
+    <div className="flex h-28 w-screen items-center justify-between bg-white px-10">
+      <Link
+        href="/"
+        className="w-1/3 text-5xl font-bold"
+      >
+        Honterview
+      </Link>
+      <ul className="flex w-1/3 justify-center gap-6 text-base font-bold">
+        {navigation.map((item) => (
+          <Link
+            href={item.url}
+            replace={item.replace}
+            scroll={item.scroll}
+            key={item.title}
+          >
+            {item.title}
+          </Link>
+        ))}
+      </ul>
+      <div className="flex w-1/3 justify-end gap-6">
+        <button type="button">마이페이지</button>
+        <button type="button">로그아웃</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
