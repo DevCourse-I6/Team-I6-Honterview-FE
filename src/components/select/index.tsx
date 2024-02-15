de11fa2c251b339aea7a02d970fe2d6133ee80a9
km6293 <alstn7616@naver.com>
2024-02-16 00:07:28 +0900

    Fix: label 삭제 uuid 추가 type 정리

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,38 +1,27 @@
 'use client';
 
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { IProps } from './types';
-
 /**
  * @brief Select 컴포넌트
  * @description
- * @param label option 추가 되는것, 필수 값 아님
  * @param options String List
  * @param value
- * @param onChange
  * @param className tailwind 요소
  */
 
-const Select = ({
-  label,
-  options,
-  value,
-  onChange,
-  className,
-  ...props
-}: IProps) => {
+const Select = ({ options, value, className, ...props }: IProps) => {
   return (
     <select
       value={value}
-      onChange={onChange}
       style={{ ...props.style }}
-      className={`${className} relative flex h-[32px] w-[55px] cursor-pointer items-center justify-center rounded-lg border border-primaries-primary bg-text-20  text-text-80 outline-none`}
+      className={`${className} relative h-[32px] cursor-pointer items-center justify-center rounded-lg border border-primaries-primary bg-text-20  text-text-80 outline-none`}
     >
-      {label && <option value="none">{label}</option>}
       {options.map((option) => (
         <option
-          key={option}
+          key={uuidv4()}
           value={option}
         >
           {option}
--- src/components/select/types.ts
+++ src/components/select/types.ts
@@ -1,9 +1,7 @@
-import { ChangeEvent, SelectHTMLAttributes } from 'react';
+import { SelectHTMLAttributes } from 'react';
 
 export interface IProps extends SelectHTMLAttributes<HTMLSelectElement> {
-  label?: string;
   options: string[];
   value: string;
-  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
   className?: string;
 }
