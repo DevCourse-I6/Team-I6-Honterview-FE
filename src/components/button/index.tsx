066813af17fa11ffb0cfce28f7347d4a683601ac
km6293 <alstn7616@naver.com>
2024-02-15 21:40:24 +0900

    Fix: 타입 종류 변경, 타입 변경

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,38 +6,34 @@ import { IProps } from './types';
 
 /**
  * @brief Button 컴포넌트
- * @description label 필수, type defalut = 1
- * @param label
- * @param type 1(Primary), 2(PrimaryAlternate), 3(Secondary), 4(SecondaryAlternate), 5(Disabled), 6(DisabledAlternate)
- * @param onClick
+ * @description children 필수, type defalut = 1
+ * @param children
+ * @param styleType 1(Primary), 2(PrimaryAlternate), 3(Secondary), 4(SecondaryAlternate), 5(Disabled), 6(DisabledAlternate)
  * @param className tailwind 요소
  */
 
-const Button = ({ label, type = 1, onClick, className, ...props }: IProps) => {
+const Button = ({ children, styleType = 1, className, ...props }: IProps) => {
   const baseStyle = `${className} relative flex h-[48px] w-[182px] flex-col items-stretch justify-center rounded-lg border`;
 
   // 각 버튼 타입에 따른 추가 스타일
   const typeStyles = {
-    1: 'bg-primaries-primary border-primaries-primary p-6 text-text-20 hover:bg-primaries-hover active:bg-primaries-active',
-    2: 'bg-text-20 border-primaries-primary p-6 text-primaries-primary',
-    3: 'bg-secondary-primary border-secondary-primary p-6 text-text-20 hover:bg-secondary-hover active:bg-secondary-active',
-    4: 'bg-text-20 border-secondary-primary p-6 text-secondary-primary',
-    5: 'bg-text-40 border-text-40 p-6 text-text-20',
-    6: 'bg-text-20 border-text-40 p-6 text-text-40',
+    1: 'bg-primaries-primary border-primaries-primary p-6 text-text-20 hover:bg-primaries-hover active:bg-primaries-active hover:border-primaries-hover active:border-primaries-active disabled:bg-text-40 disabled:text-text-20 disabled:text-opacity-50 disabled:border-text-40',
+    2: 'bg-text-20 border-primaries-primary p-6 text-primaries-primary disabled:text-text-40 disabled:border-text-40',
+    3: 'bg-secondary-primary border-secondary-primary p-6 text-text-20 hover:bg-secondary-hover active:bg-secondary-active disabled:bg-text-40 disabled:text-text-20 disabled:text-opacity-50 disabled:border-text-40',
+    4: 'bg-text-20 border-secondary-primary p-6 text-secondary-primary disabled:text-text-40 disabled:border-text-40',
   };
 
-  const buttonClass = `${baseStyle} ${typeStyles[type]}`;
+  const buttonClass = `${baseStyle} ${typeStyles[styleType]}`;
 
   return (
     <button
       type="button"
       className={buttonClass}
       style={{ ...props.style }}
-      onClick={onClick}
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
     >
-      {label}
+      {children}
     </button>
   );
 };
--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,16 +1,13 @@
-import { FormEvent, HTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 enum ButtonType {
   Type1 = 1,
   Type2 = 2,
   Type3 = 3,
   Type4 = 4,
-  Type5 = 5,
-  Type6 = 6,
 }
-export interface IProps extends HTMLAttributes<HTMLButtonElement> {
-  label: string;
-  type?: ButtonType;
-  onClick?: (() => void) | ((e: FormEvent) => void);
+export interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  styleType?: ButtonType;
   className?: string;
 }
