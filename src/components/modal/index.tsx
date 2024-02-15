25226521a67e4730c16a9fd57106becd03ccdcc6
지인혁 <dhgg321@naver.com>
2024-02-15 20:10:07 +0900

    Fix: Modal wrapper 클래스 props 추가

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -6,18 +6,23 @@ import useClickAway from '@/hooks/useClickAway';
 
 import { IProps } from './types';
 
-const Modal = ({ children, visible, onClose, ...rest }: IProps) => {
+const Modal = ({
+  children,
+  visible,
+  wrapperClassName,
+  onClose,
+  ...rest
+}: IProps) => {
   const ref = useClickAway(onClose);
 
   return createPortal(
     <div
       style={{ display: visible ? 'block' : 'none' }}
-      className="fixed left-0 top-0 z-50 h-screen w-screen bg-black/30"
+      className={`fixed left-0 top-0 z-50 h-screen w-screen bg-black/20 ${wrapperClassName}`}
     >
       <div
         ref={ref}
-        className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-        style={{ ...rest.style }}
+        className={`fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ${rest.className}`}
       >
         {children}
       </div>
--- src/components/modal/types.ts
+++ src/components/modal/types.ts
@@ -3,5 +3,6 @@ import { HTMLAttributes, ReactNode } from 'react';
 export interface IProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   visible: boolean;
+  wrapperClassName?: string;
   onClose: () => void;
 }
