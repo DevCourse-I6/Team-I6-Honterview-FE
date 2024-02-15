837470116b8104793c29329e1dbc2eb9615d1d96
km6293 <alstn7616@naver.com>
2024-02-15 23:46:16 +0900

    Fix: children 통일 XIcon 추가

--- src/components/tag/index.tsx
+++ src/components/tag/index.tsx
@@ -7,18 +7,16 @@ import { IProps } from './types';
 /**
  * @brief Tag 컴포넌트
  * @description label 필수, style 추가 수정 가능
- * @param label 제목
- * @param children 아이콘 같은 것 넣으면 될 것 같음
+ * @param children
  * @param className tailwind 요소
  */
 
-const Tag = ({ label, children, className, ...props }: IProps) => {
+const Tag = ({ children, className, ...props }: IProps) => {
   return (
     <div
-      className={`${className} flex h-[32px] w-[55px] items-center justify-center rounded-lg border-primaries-primary bg-primaries-primary text-text-20 hover:bg-primaries-hover active:bg-primaries-active`}
+      className={`${className} inline-flex items-center justify-center gap-1 rounded-lg border-primaries-primary bg-primaries-primary px-[12px] py-[5px] text-text-20 hover:bg-primaries-hover active:bg-primaries-active`}
       style={{ ...props.style }}
     >
-      {label}
       {children}
     </div>
   );
--- src/components/tag/types.ts
+++ src/components/tag/types.ts
@@ -1,7 +1,6 @@
 import { HTMLAttributes, ReactNode } from 'react';
 
 export interface IProps extends HTMLAttributes<HTMLDivElement> {
-  label: string;
   children?: ReactNode;
   className?: string;
 }
