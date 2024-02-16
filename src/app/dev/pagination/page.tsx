69042565383f331d49e39ec011ae277a9c7fed22
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-02-16 09:28:05 +0900

    Feat: 페이지네이션 컴포넌트 dev (#26)
    * Feat: /dev/pagination 페이지 예시 추가
    
    * Fix: 페이지네이션 합성 패턴으로 분리

---
+++ src/app/dev/pagination/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Pagination from '@/components/pagination';
+
+/**
+ * @brief 페이지네이션 컴포넌트
+ * @param defaultPage 초기 선택 페이지
+ * @param limit 1페이지 기준 데이터 개수
+ * @param total 총 데이터 개수
+ * @param onPageChange 선택된 페이지 변경 콜백 함수
+ */
+
+const PaginationDev = () => {
+  const printPageChange = (page: number) => {
+    console.log(page);
+  };
+
+  return (
+    <Pagination
+      defaultPage={5}
+      limit={20}
+      total={423}
+      onPageChange={printPageChange}
+    >
+      <Pagination.PrevButton />
+      <Pagination.PageButtons />
+      <Pagination.NextButton />
+    </Pagination>
+  );
+};
+
+export default PaginationDev;
