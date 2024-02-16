cd4686973b4d3e0069ad16187c1c863917850e81
nimgnas <sangminyn@gmail.com>
2024-02-17 04:34:57 +0900

    Feat: api type 추가

---
+++ src/app/dev/infiniteScroll/container/api.ts
@@ -0,0 +1,13 @@
+import { ICharactersResponse } from './types';
+
+export const fetchPosts = async (
+  page: number,
+): Promise<ICharactersResponse> => {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`,
+  );
+  if (!response.ok) {
+    throw new Error('network error');
+  }
+  return response.json();
+};
