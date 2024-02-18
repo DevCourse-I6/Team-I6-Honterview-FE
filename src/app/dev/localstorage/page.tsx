a47e930ea4e4b048aeeb27f67db680e89b128bf5
J <68155263+jxxxxe@users.noreply.github.com>
2024-02-19 01:58:50 +0900

    Feat: 로컬 스토리지 dev 페이지 생성

---
+++ src/app/dev/localstorage/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { FormEvent, useEffect, useState } from 'react';
+
+import useLocalStorage from '@/hooks/useLocalStorage/useLocalStorage';
+
+const LocalStorageDev = () => {
+  const { value, setItem, removeItem } = useLocalStorage('dev_test', '');
+  const [storedValue, setStoredValue] = useState('');
+
+  useEffect(() => {
+    setStoredValue(value);
+  }, [value]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const input = e.currentTarget.querySelector('input');
+    if (input) {
+      const inputValue = input.value;
+      setItem(inputValue);
+      input.value = '';
+    }
+  };
+
+  return (
+    <div className="flex gap-20">
+      <div className="inline-flex flex-col">
+        <div>
+          <div className="text-3xl">setItem </div>
+          <form
+            className="border-1 border-black"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              id="small-input"
+              className="mr-1 rounded-lg border border-gray-300 bg-gray-50 p-2 text-xs text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            />
+            <button
+              type="submit"
+              className="mb-2 me-2 rounded-lg border border-gray-300 bg-white px-3 py-1.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+            >
+              입력
+            </button>
+          </form>
+        </div>
+        <div>
+          <div className="text-3xl">removeItem </div>
+          <button
+            type="submit"
+            className="mb-2 me-2 rounded-lg border border-gray-300 bg-white px-3 py-1.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+            onClick={removeItem}
+          >
+            Remove
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <div className="text-3xl">value </div>
+        <div className="h-10">{storedValue}</div>
+      </div>
+    </div>
+  );
+};
+
+export default LocalStorageDev;
--- src/hooks/useLocalStorage/useLocalStorage.ts
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -4,6 +4,13 @@
 
 import { useState } from 'react';
 
+/**
+ * @brief LocalStorage 훅
+ * @description 사용할 때 value를 바로 렌더링할 수 없다 (https://nextjs.org/docs/messages/react-hydration-error)<br>
+ * 사용처에서 1.상태를 선언 2.useEffect에서 storage value 값으로 상태값 set 3. 해당 상태를 값으로 써야 함(dev page 참고)<br>
+ *
+ */
+
 const useLocalStorage = <T>(key: string, defaultValue: T) => {
   const [value, setValue] = useState<T>(() => {
     try {
