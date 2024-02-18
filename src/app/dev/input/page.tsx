1c4dc688e2a5c311eaf68fb8e0307663179e5c2a
km6293 <alstn7616@naver.com>
2024-02-19 01:04:08 +0900

    Fix: dev 수정, err, success 추가, 스타일 수정

--- src/app/dev/input/page.tsx
+++ src/app/dev/input/page.tsx
@@ -4,9 +4,26 @@ import Input from '@/components/input';
 
 const InputDev = () => {
   return (
-    <Input>
-      <Input.Text />
-    </Input>
+    <>
+      <Input>
+        <Input.Text />
+      </Input>
+      <Input
+        styleType={2}
+        className="w-1/5"
+      >
+        <Input.Text />
+      </Input>
+      <Input styleType={3}>
+        <Input.Text />
+      </Input>
+      <Input
+        styleType={3}
+        className="w-[200px]"
+      >
+        <Input.Text />
+      </Input>
+    </>
   );
 };
 
