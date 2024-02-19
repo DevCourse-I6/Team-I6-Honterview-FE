a9fa952cd581f3229448acf431a10434af77fa84
km6293 <69450660+km6293@users.noreply.github.com>
2024-02-19 16:26:28 +0900

    Feat: Input 컴포넌트 dev (#35)
    * Feat: Input dev 생성 및 props 정리
    
    * Fix: dev 수정, err, success 추가, 스타일 수정
    
    * Comment: 설명 추가

--- src/components/input/container/types.ts
+++ src/components/input/container/types.ts
@@ -1,6 +1,12 @@
 import { HTMLAttributes, ReactNode } from 'react';
 
+export enum SelectType {
+  Type1 = 1,
+  Type2 = 2,
+  Type3 = 3,
+}
 export interface IProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
+  styleType?: SelectType;
   className?: string;
 }
