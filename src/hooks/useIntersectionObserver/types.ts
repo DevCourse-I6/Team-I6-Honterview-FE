a66f502359a5d2d1e8886e07449357a851cb42b0
nimgnas <sangminyn@gmail.com>
2024-02-19 21:26:31 +0900

    Remove: 기존 intersection hook 삭제

--- src/hooks/useIntersectionObserver/types.ts
+++
@@ -1,7 +0,0 @@
-import { InfiniteQueryObserverResult } from '@tanstack/react-query';
-
-export interface IuseIntersectionObserverProps {
-  threshold?: number;
-  hasNextPage: boolean | undefined;
-  fetchNextPage: () => Promise<InfiniteQueryObserverResult>;
-}
