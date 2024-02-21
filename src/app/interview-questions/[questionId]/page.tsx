b935703e36ae03da69fdd148f1636590ad6604c3
nimgnas <sangminyn@gmail.com>
2024-02-21 11:20:26 +0900

    Design: answer 컴포넌트 - UI 구현 - favorite 아이콘 추가

--- src/app/interview-questions/[questionId]/page.tsx
+++ src/app/interview-questions/[questionId]/page.tsx
@@ -1,9 +1,11 @@
+import Answer from '@/container/interview-questions/components/Answer';
 import TitleWithInterviewStart from '@/container/interview-questions/components/TitleWithInterviewStart';
 
 const Page = () => {
   return (
     <div>
       <TitleWithInterviewStart />
+      <Answer />
     </div>
   );
 };
