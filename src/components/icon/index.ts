8729d3638427f85a1124ec5c17b80327f5a8a6bd
지인혁 <dhgg321@naver.com>
2024-02-21 10:54:14 +0900

    Feat: svg 컴포넌트 내보내기 추가

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,5 +1,11 @@
+'use client';
+
 export { default as ErrorIcon } from './svg/Error.svg';
 export { default as ExampleIcon } from './svg/example.svg';
+export { default as GithubIcon } from './svg/Github.svg';
+export { default as GoogleIcon } from './svg/Google.svg';
+export { default as KakaoIcon } from './svg/Kakao.svg';
+export { default as NaverIcon } from './svg/Naver.svg';
 export { default as SpinnerIcon } from './svg/Spinner.svg';
 export { default as SuccessIcon } from './svg/Success.svg';
 export { default as XIcon } from './svg/X.svg';
