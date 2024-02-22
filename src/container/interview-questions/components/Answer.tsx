99b068a50ae4923f64b511f7611e4d7ca565ebf8
nimgnas <sangminyn@gmail.com>
2024-02-23 02:38:41 +0900

    Design: Answer isBlur 속성 추가

--- src/container/interview-questions/components/Answer.tsx
+++ src/container/interview-questions/components/Answer.tsx
@@ -1,15 +1,17 @@
 import { FavoriteIcon } from '@/components/icon';
 
-const Answer = () => {
+import { IAnswerProps } from '../types';
+
+const Answer = ({ isBlur }: IAnswerProps) => {
   return (
-    <div>
+    <div className={`${isBlur && 'cursor-pointer select-none blur-[5px]'}`}>
       <div className="mb-5 flex justify-between">
         <h4 className="text-[1.8rem] font-semibold text-[#3182F6]">
           토스페이먼츠소속 | 기술면접마스터
         </h4>
         <FavoriteIcon />
       </div>
-      <p className="pl-14 text-[1.8rem] text-[#4e5968]">
+      <p className="pl-14 text-[1.8rem] font-light text-[#4e5968]">
         저는 React를 선호합니다. 이유는 React가 사용자 인터페이스 구축과 관리를
         쉽게 할 수 있는 사용자 측 라이브러리이기 때문입니다. 또한, 리액트
         컴포넌트는 모듈화를 통해서 개발 생산성을 높이고, 코드 유지 보수성을 높일
