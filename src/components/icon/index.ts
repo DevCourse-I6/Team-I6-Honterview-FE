c602da88973d0dcc6c86af908cf318f7f4a0e598
nimgnas <sangminyn@gmail.com>
2024-02-23 02:57:57 +0900

    Design: Bookmark 아이콘 추가

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,5 +1,7 @@
+export { default as BookmarkIcon } from './svg/Bookmark.svg';
 export { default as ErrorIcon } from './svg/Error.svg';
 export { default as ExampleIcon } from './svg/example.svg';
+export { default as FavoriteIcon } from './svg/Favorite.svg';
 export { default as SpinnerIcon } from './svg/Spinner.svg';
 export { default as SuccessIcon } from './svg/Success.svg';
 export { default as XIcon } from './svg/X.svg';
