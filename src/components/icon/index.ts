5d4d3d91db0d5b94cf6220efc37f3b9215024211
J <68155263+jxxxxe@users.noreply.github.com>
2024-02-27 03:06:50 +0900

    Design: 검색 아이콘 svg 등록

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -4,6 +4,7 @@ export { default as ArrowDownPrimary } from './svg/ArrowDownPrimary.svg';
 export { default as ArrowDownSecondary } from './svg/ArrowDownSecondary.svg';
 export { default as ArrowUpPrimary } from './svg/ArrowUpPrimary.svg';
 export { default as ArrowUpSecondary } from './svg/ArrowUpSecondary.svg';
+export { default as AutocompleteSearch } from './svg/AutocompleteSearch.svg';
 export { default as BookmarkIcon } from './svg/Bookmark.svg';
 export { default as DisabledCamera } from './svg/disabledCamera.svg';
 export { default as ErrorIcon } from './svg/Error.svg';
