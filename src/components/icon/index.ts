f3d9bf822bb5ee15fc3ce44cc3d0519a1f67e72c
nimgnas <sangminyn@gmail.com>
2024-02-27 11:34:13 +0900

    test커밋

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,10 +1,10 @@
 'use client';
 
 export { default as BookmarkIcon } from './svg/Bookmark.svg';
-export { default as FavoriteIcon } from './svg/Favorite.svg';
 export { default as DisabledCamera } from './svg/disabledCamera.svg';
 export { default as ErrorIcon } from './svg/Error.svg';
 export { default as ExampleIcon } from './svg/example.svg';
+export { default as FavoriteIcon } from './svg/Favorite.svg';
 export { default as GithubIcon } from './svg/Github.svg';
 export { default as GoogleIcon } from './svg/Google.svg';
 export { default as KakaoIcon } from './svg/Kakao.svg';
@@ -12,3 +12,5 @@ export { default as NaverIcon } from './svg/Naver.svg';
 export { default as SpinnerIcon } from './svg/Spinner.svg';
 export { default as SuccessIcon } from './svg/Success.svg';
 export { default as XIcon } from './svg/X.svg';
+
+console.log('.');
