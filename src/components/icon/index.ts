12c36cbe442cbf20e7a56e4a5f64a7be118db677
J <68155263+jxxxxe@users.noreply.github.com>
2024-02-28 10:17:42 +0900

    Rename: Icon 이름 변경

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,10 +1,10 @@
 'use client';
 
-export { default as ArrowDownPrimary } from './svg/ArrowDownPrimary.svg';
-export { default as ArrowDownSecondary } from './svg/ArrowDownSecondary.svg';
-export { default as ArrowUpPrimary } from './svg/ArrowUpPrimary.svg';
-export { default as ArrowUpSecondary } from './svg/ArrowUpSecondary.svg';
-export { default as AutocompleteSearch } from './svg/AutocompleteSearch.svg';
+export { default as ArrowDownPrimaryIcon } from './svg/ArrowDownPrimary.svg';
+export { default as ArrowDownSecondaryIcon } from './svg/ArrowDownSecondary.svg';
+export { default as ArrowUpPrimaryIcon } from './svg/ArrowUpPrimary.svg';
+export { default as ArrowUpSecondaryIcon } from './svg/ArrowUpSecondary.svg';
+export { default as SearchIcon } from './svg/AutocompleteSearch.svg';
 export { default as BookmarkIcon } from './svg/Bookmark.svg';
 export { default as DisabledCamera } from './svg/disabledCamera.svg';
 export { default as ErrorIcon } from './svg/Error.svg';
