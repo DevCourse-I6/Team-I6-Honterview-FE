9c82bbc9b9b0d9afe84b57c9ab9f6ff4e1b5ddf0
km6293 <alstn7616@naver.com>
2024-02-28 11:15:00 +0900

    Design: 디자인 수정

--- src/app/interview/chat/page.tsx
+++ src/app/interview/chat/page.tsx
@@ -7,21 +7,20 @@ import QuestionContent from '@/container/interviewChat/components/questionConten
 
 const InterviewChatPage = () => {
   return (
-    <section className="fit-wrap flex min-w-[40rem] flex-col gap-8  px-20 py-8">
-      <div className="flex grow flex-col items-center gap-4 bg-text-40 px-5 py-8">
+    <section className="flex h-[80rem] min-w-[40rem] flex-col gap-8  px-20 py-8">
+      <div className="flex grow flex-col items-center gap-4 px-5 py-8">
         <InterviewCamera />
         <QuestionContent />
         <div className="w-full items-center rounded-2xl md:w-1/2">
-          <Input className="w-full bg-white  ">
-            {/* TODO: SEACRH 아이콘 넣기 */}
+          <Input className="w-full gap-[1rem] bg-white ">
             <Input.Text
               className="py-0"
               placeholder="궁금한 점을 검색해보세요."
             />
+            <Button className="w-[8rem] px-4 py-2">보내기</Button>
           </Input>
         </div>
         <div className="flex justify-center gap-8">
-          <Button className="px-4 py-2">답변 완료</Button>
           <Button className="px-4 py-2">면접 종료</Button>
         </div>
       </div>
