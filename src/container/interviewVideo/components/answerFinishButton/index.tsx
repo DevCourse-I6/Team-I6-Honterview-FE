403f6dda527d05ae6986788ea234184c32064aad
지인혁 <dhgg321@naver.com>
2024-02-28 11:10:01 +0900

    Design: 화상 면접 페이지 컴포넌트 디자인 수정

--- src/container/interviewVideo/components/answerFinishButton/index.tsx
+++ src/container/interviewVideo/components/answerFinishButton/index.tsx
@@ -3,8 +3,9 @@ import Button from '@/components/button';
 const AnswerFinishButton = () => {
   return (
     <Button
+      type="submit"
       style={{ width: 'auto', height: 'auto', padding: '0.5rem 1rem' }}
-      className="text-small md:text-medium"
+      className="text-small text-white md:text-medium"
     >
       답변 완료
     </Button>
