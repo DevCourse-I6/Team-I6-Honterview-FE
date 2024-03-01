fe0f6068efd3e118187b37494186766898424d15
지인혁 <dhgg321@naver.com>
2024-03-01 19:24:06 +0900

    Refactor: 아이콘 컴포넌트 index.ts 파일 코드 포맷팅

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,10 +1,10 @@
 'use client';
 
 export { default as BookmarkIcon } from './svg/Bookmark.svg';
-export { default as FavoriteIcon } from './svg/Favorite.svg';
 export { default as DisabledCamera } from './svg/disabledCamera.svg';
 export { default as ErrorIcon } from './svg/Error.svg';
 export { default as ExampleIcon } from './svg/example.svg';
+export { default as FavoriteIcon } from './svg/Favorite.svg';
 export { default as GithubIcon } from './svg/Github.svg';
 export { default as GoogleIcon } from './svg/Google.svg';
 export { default as KakaoIcon } from './svg/Kakao.svg';
