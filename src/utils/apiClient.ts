fa1bb48bc3387048a655cdba5387ce53614c6e01
지인혁 <dhgg321@naver.com>
2024-03-01 18:04:55 +0900

    Fix: 유틸 fetch 함수 수정
    - body를 직렬화하는 부분을 사용한 곳으로 수정

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -2,43 +2,41 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const get = (url: string, options: RequestInit = {}) => {
   return fetch(`${API_BASE_URL}${url}`, {
-    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
     ...options,
+    method: 'GET',
   });
 };
 
 const post = (url: string, options: RequestInit = {}) => {
   return fetch(`${API_BASE_URL}${url}`, {
-    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     ...options,
-    body: JSON.stringify(options.body),
+    method: 'POST',
   });
 };
 
 const put = (url: string, options: RequestInit = {}) => {
   return fetch(`${API_BASE_URL}${url}`, {
-    method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
     ...options,
-    body: JSON.stringify(options.body),
+    method: 'PUT',
   });
 };
 
 const del = (url: string, options: RequestInit = {}) => {
   return fetch(`${API_BASE_URL}${url}`, {
-    method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
     ...options,
+    method: 'DELETE',
   });
 };
 
