17e1df6af7218637a4bac5f50ed373825ba3790a
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-04 06:57:39 +0900

    Feature: 전체 레이아웃 구현

--- src/container/presetting/components/sceneSection/components/firstQuestionScene/index.tsx
+++ src/container/presetting/components/sceneSection/components/firstQuestionScene/index.tsx
@@ -1,15 +1,43 @@
 import { useEffect } from 'react';
 
-import Input from '@/components/input';
+import usePresettingDataStore from '@/container/presetting/stores/usePresettingDataStore';
 import useStepStore from '@/container/presetting/stores/useStepStore';
 
+import QuestionSection from './components/questionSection';
+import TagSection from './components/tagSection';
+
 const FirstQuestionScene = () => {
-  const { setNextButtonOn } = useStepStore();
+  const { setNextButtonOn, setNextButtonOff } = useStepStore();
+  const { firstQuestionTags, firstQuestion } = usePresettingDataStore();
 
   useEffect(() => {
-    setNextButtonOn();
-  }, [setNextButtonOn]);
-  return <Input>질문을 선택하세요</Input>;
+    if (firstQuestionTags.length && firstQuestion) {
+      setNextButtonOn();
+      return;
+    }
+    setNextButtonOff();
+  }, [
+    firstQuestion,
+    firstQuestionTags.length,
+    setNextButtonOff,
+    setNextButtonOn,
+  ]);
+
+  return (
+    <div className="flex h-full flex-col justify-center gap-[5rem]">
+      <div>
+        <p className="text-text-60">
+          선택한 질문과 이에 대한 꼬리질문들로 면접이 진행됩니다
+        </p>
+      </div>
+      <div className="flex flex-col justify-center gap-[5rem]">
+        <TagSection />
+        <div className="h-[14rem]">
+          {firstQuestionTags.length > 0 && <QuestionSection />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FirstQuestionScene;
