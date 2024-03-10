de23566dfb1b5ab33f34e12682d23092dd1d6c08
지인혁 <dhgg321@naver.com>
2024-03-10 23:04:09 +0900

    Refactor: apiClient 유틸 함수 리팩토링

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable unused-imports/no-unused-vars */
-import { cookies } from 'next/headers';
-
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const getAccessToken = () => {
@@ -14,14 +10,6 @@ const getAccessToken = () => {
 };
 
 const get = (url: string, options: RequestInit = {}) => {
-  let accessToken = null;
-
-  if (typeof window === 'undefined') {
-    const cookieStore = cookies();
-
-    accessToken = cookieStore.get('accessToken');
-  }
-
   return fetch(`${API_BASE_URL}${url}`, {
     ...options,
     method: 'GET',
@@ -35,13 +23,6 @@ const get = (url: string, options: RequestInit = {}) => {
 };
 
 const post = (url: string, options: RequestInit = {}) => {
-  let accessToken = null;
-
-  if (typeof window === 'undefined') {
-    const cookieStore = cookies();
-    accessToken = cookieStore.get('accessToken');
-  }
-
   return fetch(`${API_BASE_URL}${url}`, {
     ...options,
     method: 'POST',
@@ -55,13 +36,6 @@ const post = (url: string, options: RequestInit = {}) => {
 };
 
 const put = (url: string, options: RequestInit = {}) => {
-  let accessToken = null;
-
-  if (typeof window === 'undefined') {
-    const cookieStore = cookies();
-    accessToken = cookieStore.get('accessToken');
-  }
-
   return fetch(`${API_BASE_URL}${url}`, {
     ...options,
     method: 'PUT',
@@ -75,13 +49,6 @@ const put = (url: string, options: RequestInit = {}) => {
 };
 
 const del = (url: string, options: RequestInit = {}) => {
-  let accessToken = null;
-
-  if (typeof window === 'undefined') {
-    const cookieStore = cookies();
-    accessToken = cookieStore.get('accessToken');
-  }
-
   return fetch(`${API_BASE_URL}${url}`, {
     ...options,
     method: 'DELETE',
@@ -95,13 +62,6 @@ const del = (url: string, options: RequestInit = {}) => {
 };
 
 const patch = (url: string, options: RequestInit = {}) => {
-  let accessToken = null;
-
-  if (typeof window === 'undefined') {
-    const cookieStore = cookies();
-    accessToken = cookieStore.get('accessToken');
-  }
-
   return fetch(`${API_BASE_URL}${url}`, {
     ...options,
     method: 'PATCH',
