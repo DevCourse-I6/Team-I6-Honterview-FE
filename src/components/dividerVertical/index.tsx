69e0c42d2621f49b0230e104a5c01ef55b00f7a8
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-13 10:15:08 +0900

    Design: 마이페이지 전체 레이아웃 구현 (#77)
    * Chore: 반응형 기기 사이즈 설정
    
    * Design: 디바이더 vertical 추가
    
    * Feat: 헤더 로그인 연결 및 마이페이지 추가
    
    * Design: 마이페이지 전체 레이아웃 구현
    
    * Design: 로그아웃 버튼 수정
    
    * Fix: 타입 에러 수정
    
    * Refactor: 섹션 분리
    
    * Fix: 빌드 에러 수정(반응형)

---
+++ src/components/dividerVertical/index.tsx
@@ -0,0 +1,12 @@
+import { twMerge } from 'tailwind-merge';
+
+import { DividerVerticalProps } from './type';
+
+const DividerVertical = ({ className }: DividerVerticalProps) => {
+  const style = twMerge(
+    `w-[0.05rem] inline-block max-h-[100vh-9rem-7rem] self-stretch border-t-0 bg-neutral-400 opacity-100 dark:opacity-50 ${className} `,
+  );
+  return <div className={style} />;
+};
+
+export default DividerVertical;
