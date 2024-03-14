f86b75ba6782ea66f6649761af58c4246c6ae5ae
지인혁 <dhgg321@naver.com>
2024-03-15 00:23:55 +0900

    Feat: 로그인 페이지 메타태그 설정

--- src/app/auth/login/layout.tsx
+++ src/app/auth/login/layout.tsx
@@ -2,7 +2,16 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: '혼터뷰 - 로그인',
-  description: '혼터뷰에 로그인하여 서비스를 이용하세요',
+  description: '혼터뷰에 로그인하여 더욱 더 다양한 서비스를 이용하세요',
+  openGraph: {
+    title: '혼터뷰 - 로그인',
+    description: '혼터뷰에 로그인하여 더욱 더 다양한 서비스를 이용하세요',
+    url: `http://honterview.site/auth/login`,
+  },
+  twitter: {
+    title: '혼터뷰 - 로그인',
+    description: '혼터뷰에 로그인하여 더욱 더 다양한 서비스를 이용하세요',
+  },
 };
 
 const AuthLoginLayout = ({
