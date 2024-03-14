4655df299a22dff49ac9170835d54bada5c36248
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-14 11:38:40 +0900

    Design: 닉네임 섹션 디자인 개선

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -4,17 +4,15 @@ import NickNameSection from '@/container/mypage/components/nickNameSection';
 
 const MyPage = () => {
   return (
-    <>
-      <div className="wrap flex flex-col items-center pt-[1rem]">
-        <NickNameSection />
-        <div className="flex h-full flex-1 flex-col items-center gap-[4rem]">
-          <ContentSection />
-          <div className="mb-[2rem] flex justify-center">
-            <LogoutSection />
-          </div>
+    <div className="wrap flex flex-col items-center pt-[1rem]">
+      <NickNameSection />
+      <div className="flex h-full flex-1 flex-col items-center gap-[4rem]">
+        <ContentSection />
+        <div className="mb-[2rem] flex justify-center">
+          <LogoutSection />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
