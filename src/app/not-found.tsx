e34be26a53af61dfc2f90376712ab25b079f3972
지인혁 <dhgg321@naver.com>
2024-03-15 00:22:35 +0900

    Design: not-found 페이지 디자인 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,42 @@
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import Button from '@/components/button';
+import Logo from '@/components/logo';
+
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  const router = useRouter();
+
+  const handlePrevPage = () => {
+    router.back();
+  };
+
+  return (
+    <div className="fit-wrap flex flex-col items-center justify-center gap-16">
+      <Logo
+        width={150}
+        height={150}
+      />
+      <h1 className="text-center text-tripleLarge">
+        존재하지 않는 페이지입니다.
+      </h1>
+      <div className="flex items-center gap-16">
+        <Link href="/">
+          <Button className="h-auto w-auto px-[1rem] py-[0.5rem] text-small text-white md:text-medium">
+            메인 페이지
+          </Button>
+        </Link>
+        <Button
+          onClick={handlePrevPage}
+          className="h-auto w-auto px-[1rem] py-[0.5rem] text-small text-white md:text-medium"
+        >
+          이전 페이지
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
