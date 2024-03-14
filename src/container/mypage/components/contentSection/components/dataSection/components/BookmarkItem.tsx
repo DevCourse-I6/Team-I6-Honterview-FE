a2d68f175c3363ae5695c7be09c81dd32441f014
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-14 20:10:00 +0900

    Fix: 북마크 클릭 시 질문 링크로 이동하지 않도록 수정

--- src/container/mypage/components/contentSection/components/dataSection/components/BookmarkItem.tsx
+++ src/container/mypage/components/contentSection/components/dataSection/components/BookmarkItem.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -8,10 +8,18 @@ import { ContentType } from '../type';
 
 const BookmarkItem = ({ id, title, categoryList }: ContentType) => {
   const [isBookmarked, setIsBookmarked] = useState(true);
+  const route = useRouter();
+
+  const handleBookmarkClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setIsBookmarked((state) => !state);
+  };
+
   return (
-    <Link
-      href={`/questions/${id}`}
-      className="flex gap-[2rem] p-[1rem]"
+    <div
+      onClick={() => route.push(`/questions/${id}`)}
+      className="flex cursor-pointer items-center gap-[2rem] p-[1rem]"
+      role="presentation"
     >
       <div className="flex flex-col gap-[0.8rem] rounded-lg">
         <div className="flex gap-[1rem]">
@@ -24,22 +32,22 @@ const BookmarkItem = ({ id, title, categoryList }: ContentType) => {
             </div>
           ))}
         </div>
-        <div className="pl-[0.5rem] text-[1.5rem] tablet:text-[1.7rem]">
+        <div className="flex pl-[0.5rem] text-[1.5rem] tablet:text-[1.7rem]">
           <span className="whitespace-pre text-text-60">Q. </span>
-          {title}
+          <div className="flex justify-start">{title}</div>
         </div>
       </div>
       <div className="flex flex-1 items-center justify-end">
         <button
           type="button"
-          onClick={() => setIsBookmarked((state) => !state)}
+          onClick={handleBookmarkClick}
         >
           <BookmarkSimpleIcon
             className={`h-[2.5rem] w-[2.5rem] stroke-primaries-primary ${isBookmarked && 'fill-primaries-primary'}`}
           />
         </button>
       </div>
-    </Link>
+    </div>
   );
 };
 
