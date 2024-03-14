bd165564d0519a6695c85938bfb60aa6880c9a19
지인혁 <dhgg321@naver.com>
2024-03-15 01:09:55 +0900

    Fix: 메타태그 기본 url 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import MswProvider from '@/mocks/mswProvider';
 import ReactQueryProvider from '@/utils/ReactQueryProvider';
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.METADATA_BASE!),
   title: '혼터뷰',
   description: `혼터뷰와 함께 AI 기반 면접 연습을 통해 면접 실력을 강화하세요. 실제 면접 상황처럼 준비하며 자신감을 키우고, 면접 능력을 향상시켜 보세요.`,
   icons: {
