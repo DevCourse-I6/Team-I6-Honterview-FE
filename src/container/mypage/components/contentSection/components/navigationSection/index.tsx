8c1e966d0433ce2698078142b04b22635dad8bfe
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-14 11:41:43 +0900

    Design: 메뉴 섹션 디자인 개선

--- src/container/mypage/components/contentSection/components/navigationSection/index.tsx
+++ src/container/mypage/components/contentSection/components/navigationSection/index.tsx
@@ -4,22 +4,24 @@ const NavigationSection = ({ activeMenu, onClick }: NavigationSectionProps) => {
   const isResultActive = activeMenu === 'result';
 
   return (
-    <nav className="mb-[2rem] flex w-full justify-around text-[2rem] laptop:justify-center laptop:gap-[25rem]">
-      <button
-        type="button"
-        className={`tablet:flex tablet:w-[11rem] tablet:justify-end ${isResultActive ? 'font-medium text-primaries-primary' : 'text-text-60'}`}
-        onClick={() => onClick('result')}
-      >
-        연습 기록
-      </button>
-      <button
-        type="button"
-        className={`w-[11rem] ${!isResultActive ? 'font-medium text-primaries-primary' : 'text-text-60'}`}
-        onClick={() => onClick('bookmark')}
-      >
-        북마크한 질문
-      </button>
-    </nav>
+    <div className="w-screen tablet:w-[50rem]">
+      <nav className="flex w-full justify-center gap-[10rem] pb-[1rem] text-[1.7rem] tablet:w-[50rem] tablet:gap-[15rem] tablet:pb-[2rem] tablet:text-[2rem]">
+        <button
+          type="button"
+          className={`${isResultActive ? 'font-medium text-primaries-primary underline underline-offset-[0.7rem]' : 'text-text-60'}`}
+          onClick={() => onClick('result')}
+        >
+          연습 기록
+        </button>
+        <button
+          type="button"
+          className={`${!isResultActive ? 'font-medium text-primaries-primary underline underline-offset-[0.57rem]' : 'text-text-60'}`}
+          onClick={() => onClick('bookmark')}
+        >
+          북마크한 질문
+        </button>
+      </nav>
+    </div>
   );
 };
 
