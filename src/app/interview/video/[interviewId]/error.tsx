0f71d0c39334c15c92e2d9641a1bf534354fcac5
지인혁 <dhgg321@naver.com>
2024-03-15 00:31:03 +0900

    Design: 인터뷰 화상 오류 페이지 수정

--- src/app/interview/video/[interviewId]/error.tsx
+++ src/app/interview/video/[interviewId]/error.tsx
@@ -1,26 +1,51 @@
 'use client';
 
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 import Button from '@/components/button';
+import Logo from '@/components/logo';
 import { notify } from '@/components/toast';
 import { IErrorPageProps } from '@/types/errorPage';
 
 const Error = ({ error, reset }: IErrorPageProps) => {
+  const router = useRouter();
+
+  const handlePrevPage = () => {
+    router.back();
+  };
+
   useEffect(() => {
     notify('error', error.message);
   }, [error]);
 
   return (
-    <div className="fit-wrap flex flex-col items-center justify-center gap-6 text-tripleLarge">
-      <h2>화상 인터뷰 페이지에 문제가 발생했습니다.</h2>
-      <Button
-        style={{ width: 'auto', height: 'auto', padding: '0.5rem 1rem' }}
-        className="h-auto w-auto px-[0.5] py-4 text-medium text-white md:text-medium"
-        onClick={() => reset()}
-      >
-        재시도
-      </Button>
+    <div className="fit-wrap flex flex-col items-center justify-center gap-16">
+      <Logo
+        width={150}
+        height={150}
+      />
+      <h1 className="text-center text-tripleLarge">인터뷰 페이지 오류 발생</h1>
+      <div className="flex items-center gap-16">
+        <Link href="/">
+          <Button className="h-auto w-auto px-[1rem] py-[0.5rem] text-small text-white md:text-medium">
+            메인 페이지
+          </Button>
+        </Link>
+        <Button
+          onClick={handlePrevPage}
+          className="h-auto w-auto px-[1rem] py-[0.5rem] text-small text-white md:text-medium"
+        >
+          이전 페이지
+        </Button>
+        <Button
+          onClick={reset}
+          className="h-auto w-auto px-[1rem] py-[0.5rem] text-small text-white md:text-medium"
+        >
+          재시도
+        </Button>
+      </div>
     </div>
   );
 };
