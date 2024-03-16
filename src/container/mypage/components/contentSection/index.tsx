b9ccdd10e0a5d23375e744f4f084beff6af39fc6
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-17 03:29:34 +0900

    Design: 첫페이지와 끝페이지일 때 페이지네이션 이전,다음 버튼 없앰

--- src/container/mypage/components/contentSection/index.tsx
+++ src/container/mypage/components/contentSection/index.tsx
@@ -40,9 +40,17 @@ const ContentSection = () => {
           onPageChange={setCurrentBookmarkPage}
           className={`${(!isBookmarkOn || !bookmarkItemCount) && 'hidden'} gap-[1rem] tablet:gap-6`}
         >
-          <Pagination.PrevButton className="w-[1rem]" />
+          {currentBookmarkPage > 1 ? (
+            <Pagination.PrevButton className="w-[1rem]" />
+          ) : (
+            <span className="w-[3rem]" />
+          )}
           <Pagination.PageButtons className="h-[3rem] min-w-[3rem] py-0" />
-          <Pagination.NextButton />
+          {currentBookmarkPage < Math.ceil(bookmarkItemCount / 10) ? (
+            <Pagination.NextButton className="flex w-[3rem] items-center justify-start" />
+          ) : (
+            <span className="w-[3rem]" />
+          )}
         </Pagination>
       </>
       <>
@@ -56,11 +64,19 @@ const ContentSection = () => {
           limit={10}
           total={resultItemCount}
           onPageChange={setCurrentResultPage}
-          className={`${(!isResultOn || !resultItemCount) && 'hidden'} gap-[0.5rem] tablet:gap-6`}
+          className={`${(!isResultOn || !resultItemCount) && 'hidden'} gap-[0.4rem] text-medium tablet:gap-6 tablet:text-large`}
         >
-          <Pagination.PrevButton className="w-[1rem]" />
-          <Pagination.PageButtons className="h-[3rem] min-w-[3rem] py-0" />
-          <Pagination.NextButton />
+          {currentResultPage > 1 ? (
+            <Pagination.PrevButton className="flex w-[3rem] items-center justify-end" />
+          ) : (
+            <span className="w-[3rem]" />
+          )}
+          <Pagination.PageButtons className="flex h-[3rem] min-w-[3rem] items-center justify-center py-0 " />
+          {currentResultPage < Math.ceil(resultItemCount / 10) ? (
+            <Pagination.NextButton className="flex w-[3rem] items-center justify-start" />
+          ) : (
+            <span className="w-[3rem]" />
+          )}
         </Pagination>
       </>
     </div>
