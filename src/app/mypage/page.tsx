7d60bb2603868157aa39f13f183bc02d861a5e6e
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-17 03:26:32 +0900

    Remove: 로그아웃 삭제

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,16 +1,12 @@
 import ContentSection from '@/container/mypage/components/contentSection';
-import LogoutSection from '@/container/mypage/components/logoutSection';
 import NickNameSection from '@/container/mypage/components/nickNameSection';
 
 const MyPage = () => {
   return (
     <div className="wrap relative flex flex-col items-center pt-[1rem]">
       <NickNameSection />
-      <div className="mb-[8rem] flex h-full flex-1 flex-col items-center">
+      <div className="mb-[4rem] flex h-full flex-1 flex-col items-center">
         <ContentSection />
-      </div>
-      <div className="absolute bottom-[2rem] flex justify-center">
-        <LogoutSection />
       </div>
     </div>
   );
