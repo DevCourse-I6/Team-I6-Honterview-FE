73101a4bbafc8c8d4d786b3edbe8958543aa60b6
지인혁 <dhgg321@naver.com>
2024-03-17 21:56:25 +0900

    Feat: 헤더 반응형 작업 추가

--- src/components/Header/components/hamburgerMenu/index.tsx
+++ src/components/Header/components/hamburgerMenu/index.tsx
@@ -1,12 +1,31 @@
+'use client';
+
+import { useState } from 'react';
+
 import { HamburgerIcon } from '@/components/icon';
 
-const HamburgerMenu = () => {
+import { IProps } from './components/types';
+
+const HamburgerMenu = ({ children }: IProps) => {
+  const [active, setActive] = useState(false);
+
+  const handleToggleActive = () => {
+    setActive((prev) => !prev);
+  };
+
   return (
-    <div className="block text-[3rem] md:hidden">
-      <button type="button">
-        <HamburgerIcon />
-      </button>
-    </div>
+    <>
+      <div className="block text-[3rem] md:hidden">
+        <button
+          type="button"
+          onClick={handleToggleActive}
+          className="flex items-center justify-center"
+        >
+          <HamburgerIcon />
+        </button>
+      </div>
+      {active && children}
+    </>
   );
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,24 @@
+import DividerHorizontal from '../dividerHorizontal';
 import HamburgerMenu from './components/hamburgerMenu';
+import MobileLinkButtons from './components/hamburgerMenu/components/mobileLinkButtons';
+import MobileNavigation from './components/hamburgerMenu/components/mobileNavigation';
 import LinkButtons from './components/linkButtons';
 import Navigation from './components/navigation';
 import Title from './components/title';
 
 const Header = async () => {
   return (
-    <header className="flex h-28 flex-wrap items-center justify-between px-8 md:justify-around">
+    <header className="flex h-28 min-w-[32rem] flex-wrap items-center justify-between px-8 md:justify-around">
       <Title />
       <Navigation />
       <LinkButtons />
-      <HamburgerMenu />
+      <HamburgerMenu>
+        <div className="absolute right-0 top-[7rem] z-10 flex flex-col border text-medium font-medium md:hidden">
+          <MobileNavigation />
+          <DividerHorizontal />
+          <MobileLinkButtons />
+        </div>
+      </HamburgerMenu>
     </header>
   );
 };
