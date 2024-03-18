8df97ad9e0c8f619ecec39f7875e70d88edff953
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-18 11:20:40 +0900

    Feat: 닉네임 수정 모달 창 구현

---
+++ src/container/mypage/components/nickNameSection/components/NicknameEditSection/nicknameEditSection.tsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+
+import Button, { ButtonType } from '@/components/button';
+import { XIcon } from '@/components/icon';
+import Input from '@/components/input';
+import { notify } from '@/components/toast';
+import { editMyNickname } from '@/services/mypage';
+
+import { NicknameEditSectionProps } from './type';
+
+const NicknameEditSection = ({
+  currentNickname,
+  closeModal,
+  onChangeNickname,
+}: NicknameEditSectionProps) => {
+  const [nickname, setNickname] = useState(currentNickname);
+  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const regex = /^[가-힣a-zA-Z0-9\\s]{2,20}$/;
+    if (nickname.length < 2 || nickname.length > 20) {
+      notify('warning', '닉네임은 최소 2글자, 최대 20글자여야 합니다');
+      return;
+    }
+    if (!regex.test(nickname)) {
+      notify('warning', '닉네임은 한글, 숫자, 영문으로만 이루어져야 합니다');
+      return;
+    }
+    if (nickname === currentNickname) {
+      notify('warning', '닉네임을 변경해 주세요');
+      return;
+    }
+    editMyNickname(nickname);
+    notify('info', '변경되었습니다!');
+    onChangeNickname(nickname);
+    closeModal();
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setNickname(value);
+  };
+
+  return (
+    <div className="relative flex h-[20rem] w-[28rem] flex-col items-center gap-[2rem] rounded-2xl bg-white p-[2rem] text-[1.5rem] tablet:w-[30rem]">
+      <h2 className="flex select-none text-[2rem] font-semibold">
+        닉네임 변경
+      </h2>
+      <div className="flex flex-col gap-[0.5rem]">
+        <div className="w-full text-[1rem] text-text-40">
+          한글, 영문, 숫자 입력 가능 (2~20자)
+        </div>
+        <form
+          className="flex justify-center gap-[0.7rem]"
+          onSubmit={onSubmit}
+        >
+          <Input className="h-[3rem] w-full border">
+            <Input.Text
+              value={nickname}
+              onChange={onChange}
+              className="w-fit"
+            />
+            <button
+              type="button"
+              onClick={() => setNickname('')}
+            >
+              <XIcon
+                className={`stroke-black ${!nickname && 'hidden'}`}
+                width="1.5rem"
+              />
+            </button>
+          </Input>
+          <div className="absolute bottom-[2rem] flex gap-[1rem]">
+            <Button
+              styleType={ButtonType.Type3}
+              className="h-[3rem] w-[7rem] px-0 text-[1.3rem]"
+            >
+              취소
+            </Button>
+            <Button
+              styleType={ButtonType.Type1}
+              className="h-[3rem] w-[7rem] px-0 text-[1.3rem]"
+              type="submit"
+            >
+              확인
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NicknameEditSection;
--- src/container/mypage/components/nickNameSection/index.tsx
+++ src/container/mypage/components/nickNameSection/index.tsx
@@ -3,14 +3,18 @@
 import { useEffect, useState } from 'react';
 
 import { ThreeDotsIcon } from '@/components/icon';
+import Modal from '@/components/modal';
 import { notify } from '@/components/toast';
 import useClickAway from '@/hooks/useClickAway';
 import { getMyInfo } from '@/services/mypage';
+
+import NicknameEditSection from './components/NicknameEditSection/nicknameEditSection';
 
 const NickNameSection = () => {
   const [nickname, setNickname] = useState('');
   const [isPopoverOn, setIsPopoverOn] = useState(false);
   const ref = useClickAway(() => setIsPopoverOn(false));
+  const [editNicknameModalOn, setEditNicknameModalOn] = useState(false);
   const onPopoverClick = () => {
     setIsPopoverOn(true);
   };
@@ -39,11 +43,22 @@ const NickNameSection = () => {
         <ThreeDotsIcon className="h-[1.5rem] w-fit" />
         <div
           className={`${!isPopoverOn && 'hidden'} z-10 flex h-[3rem] w-[10rem] translate-x-[-40%] items-center justify-center rounded-lg border-[0.1rem] border-gray-200 bg-gray-700 text-[1.2rem] text-white shadow-sm tablet:translate-x-0`}
+          onClick={() => setEditNicknameModalOn(true)}
           role="presentation"
         >
           닉네임 변경
         </div>
       </button>
+      <Modal
+        visible={editNicknameModalOn}
+        onClose={() => setEditNicknameModalOn(false)}
+      >
+        <NicknameEditSection
+          currentNickname={nickname}
+          closeModal={() => setEditNicknameModalOn(false)}
+          onChangeNickname={(name) => setNickname(name)}
+        />
+      </Modal>
     </div>
   );
 };
