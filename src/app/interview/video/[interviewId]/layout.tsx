f6744e9420fdac36236f172cc50106be7feb0a77
지인혁 <dhgg321@naver.com>
2024-03-18 13:26:18 +0900

    Feat: 모든 페이지 메타태그 설정

--- src/app/interview/video/[interviewId]/layout.tsx
+++ src/app/interview/video/[interviewId]/layout.tsx
@@ -11,18 +11,20 @@ export const generateMetadata = async ({
   const { data } = await getInterviewInfo(interviewId);
   const { categoryNames, questionsAndAnswers } = data;
   const { questionContent } = questionsAndAnswers[0];
+  const title = '혼터뷰 - 화상 면접 연습';
+  const description = `혼터뷰에서는 ${categoryNames.join(', ')} 카테고리의 첫 번째 질문인 '${questionContent}'을 시작으로 화상 면접 연습을 제공합니다. 실제 면접 상황처럼 준비하며 자신감을 키우고, 면접 능력을 향상시켜 보세요.`;
 
   return {
-    title: '혼터뷰 - 화상 면접 연습',
-    description: `혼터뷰에서는 ${categoryNames.join(', ')} 카테고리의 첫 번째 질문인 '${questionContent}'을 시작으로 화상 면접 연습을 제공합니다. 실제 면접 상황처럼 준비하며 자신감을 키우고, 면접 능력을 향상시켜 보세요.`,
+    title,
+    description,
     openGraph: {
-      title: '혼터뷰 - 화상 면접 연습',
-      description: `혼터뷰에서는 ${categoryNames.join(', ')} 카테고리의 첫 번째 질문인 '${questionContent}'을 시작으로 화상 면접 연습을 제공합니다. 실제 면접 상황처럼 준비하며 자신감을 키우고, 면접 능력을 향상시켜 보세요.`,
+      title,
+      description,
       url: `http://honterview.site/interview/video/${interviewId}`,
     },
     twitter: {
-      title: '혼터뷰 - 화상 면접 연습',
-      description: `혼터뷰에서는 ${categoryNames.join(', ')} 카테고리의 첫 번째 질문인 '${questionContent}'을 시작으로 화상 면접 연습을 제공합니다. 실제 면접 상황처럼 준비하며 자신감을 키우고, 면접 능력을 향상시켜 보세요.`,
+      title,
+      description,
     },
   };
 };
