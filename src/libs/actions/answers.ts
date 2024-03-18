496edf6ee5737e903ebdee4cba35dab35f68c361
nimgnas <sangminyn@gmail.com>
2024-03-19 01:24:28 +0900

    Feat: answerHeartClick servarAction으로 변경

---
+++ src/libs/actions/answers.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+import { apiServer } from '@/utils/apiServer';
+
+import { IClickAnswerHeart } from '../types/response';
+import { reissueAccessToken } from './auth';
+
+export const clickAnswerHeart = async (
+  answerId: number,
+): Promise<IClickAnswerHeart> => {
+  const response = await apiServer.post(`api/v1/answers/${answerId}/hearts`, {
+    cache: 'no-store',
+  });
+
+  if (response.status === 401) {
+    return reissueAccessToken(() => clickAnswerHeart(answerId));
+  }
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    cookies().set('errorMessage', errorData.errorMessage, {
+      maxAge: 5,
+    });
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
