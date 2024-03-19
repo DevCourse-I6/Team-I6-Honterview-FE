399f90abde49cb7e962688e44fb1d4e9e304614d
km6293 <alstn7616@naver.com>
2024-03-20 03:58:53 +0900

    Feat: camera 추가

--- src/container/interviewChat/components/interviewCamera/index.tsx
+++ src/container/interviewChat/components/interviewCamera/index.tsx
@@ -2,38 +2,28 @@
 
 import React, { useState } from 'react';
 
-import MirrorView from '@/components/camera/components/MirrorView';
 import { DisabledCamera } from '@/components/icon';
 import Toggle from '@/components/toggle';
-import useVideoCheckScene from '@/container/presetting/components/sceneSection/components/videoCheckScene/useVideoCheckScene';
+
+import ChatMirrorView from './ChatMirrorView';
 
 const InterviewCamera = () => {
-  const { previewStream, isLoading, error } = useVideoCheckScene();
-  const [camerraToggle, setCamerraToggle] = useState(true);
+  const [camerraToggle, setCamerraToggle] = useState(false);
 
   return (
-    <div className="relative h-[20rem] w-[40rem] rounded-2xl bg-background-20">
-      {!isLoading && camerraToggle ? (
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+    <div className="relative h-[20rem] w-[35rem] rounded-2xl bg-background-20">
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <div className="absolute">
+          {camerraToggle ? (
+            <ChatMirrorView camerraToggle={camerraToggle} />
+          ) : (
             <DisabledCamera className="h-[5rem] w-[5rem]" />
-          </div>
-          <div className="absolute bottom-0 right-0 m-2">
-            <Toggle onChange={() => setCamerraToggle(!camerraToggle)} />
-          </div>
+          )}
         </div>
-      ) : (
-        <>
-          <MirrorView
-            stream={previewStream}
-            isLoading={isLoading}
-            error={error}
-          />
-          <div className="absolute bottom-0 right-0 m-2">
-            <Toggle onChange={() => setCamerraToggle(!camerraToggle)} />
-          </div>
-        </>
-      )}
+        <div className="absolute bottom-0 right-0 m-2">
+          <Toggle onChange={() => setCamerraToggle(!camerraToggle)} />
+        </div>
+      </div>
     </div>
   );
 };
