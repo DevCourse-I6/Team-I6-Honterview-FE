d9d91f0f8f52b0785fd7c4a3974a5f499e5eca74
nimgnas <sangminyn@gmail.com>
2024-03-19 18:50:19 +0900

    Feat: 상세페이지 isAdmin 조건문 추가

--- src/container/questions/components/TitleWithInterviewStart/components/HeaderButton/index.tsx
+++ src/container/questions/components/TitleWithInterviewStart/components/HeaderButton/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+import { getUserAuth } from '@/libs/services/auth';
 
 import BookmarkButton from './components/BookmarkButton';
 import DeleteButton from './components/DeleteButton';
@@ -6,7 +6,7 @@ import HeartButton from './components/HeartButton';
 import UpdateButton from './components/UpdateButton';
 import { IProps } from './types';
 
-const HeaderButton = ({
+const HeaderButton = async ({
   questionId,
   questionTitle,
   categoryNames,
@@ -15,18 +15,23 @@ const HeaderButton = ({
   isHearted,
   isBookmarked,
 }: IProps) => {
+  const userAuth = await getUserAuth();
+  const isAdmin = userAuth?.data?.role === 'ROLE_ADMIN';
+
   return (
-    <div className="flex justify-between">
-      <div className="flex gap-4">
-        <UpdateButton
-          questionId={questionId}
-          questionTitle={questionTitle}
-          categoryNames={categoryNames}
-          categories={categories}
-        />
-        <DeleteButton questionId={questionId} />
-      </div>
-      <div className="flex items-center gap-3">
+    <div className={`flex ${isAdmin ? 'justify-between' : 'justify-end'}`}>
+      {isAdmin && (
+        <div className="flex gap-4">
+          <UpdateButton
+            questionId={questionId}
+            questionTitle={questionTitle}
+            categoryNames={categoryNames}
+            categories={categories}
+          />
+          <DeleteButton questionId={questionId} />
+        </div>
+      )}
+      <div className="flex items-center justify-center">
         <HeartButton
           questionId={questionId}
           isHearted={isHearted}
