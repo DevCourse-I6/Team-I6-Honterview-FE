55e192c739c28ce8edb3b116ce4ab2bc222c7028
J <68155263+jxxxxe@users.noreply.github.com>
2024-03-20 20:31:06 +0900

    Design: 면접전 첫화면의 설명을 툴팁으로 UI 변경

--- src/container/presetting/components/stepSection/index.tsx
+++ src/container/presetting/components/stepSection/index.tsx
@@ -1,3 +1,5 @@
+import './style.css';
+
 import { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -22,7 +24,7 @@ const StepSection = () => {
   }, [interviewType, setCameraStep, setChattingStep]);
 
   return (
-    <div className="relative flex w-full justify-center text-[1.4rem] font-semibold tablet:h-[12rem] tablet:items-center tablet:px-[3rem] tablet:text-[1.6rem]">
+    <div className="relative flex w-full justify-center gap-[1rem] text-[1.3rem] font-semibold tablet:h-[12rem] tablet:items-center tablet:px-[3rem] tablet:text-[1.6rem]">
       {Array.from({ length: totalStep }, (_, i) => i + 1).map((step) => (
         <div
           className="flex items-center justify-center"
@@ -33,13 +35,23 @@ const StepSection = () => {
               number={step}
               isPassed={currentStep >= step}
             />
-            <span
-              className={`flex w-[9.5rem] items-center justify-center 
+            <div className="relative flex w-[8rem] items-center justify-center gap-[0.3rem] tablet:w-[9.5rem]">
+              <span
+                className={`flex items-center justify-center 
               ${currentStep === step ? 'text-primaries-primary' : 'text-text-80'}
             `}
-            >
-              {TITLE_LIST[step as StepNumber]}
-            </span>
+              >
+                {TITLE_LIST[step as StepNumber]}
+              </span>
+              {step === 1 && (
+                <span className="tooltip-mark flex h-[1.4rem] w-[1.4rem] select-none items-center justify-center rounded-full border-[0.13rem] border-text-80 text-[1.1rem] text-text-80 tablet:h-[1.5rem] tablet:w-[1.5rem] tablet:text-[1.2rem]">
+                  <div className="tooltip-box invisible absolute left-0 top-[120%] z-10 whitespace-nowrap rounded-lg bg-gray-900 px-3 py-2 text-[1.5rem] font-medium text-white opacity-0 shadow-sm transition-opacity duration-300">
+                    첫 질문과 이에 대한 꼬리 질문들로 면접이 진행됩니다
+                  </div>
+                  ?
+                </span>
+              )}
+            </div>
           </div>
           {step !== totalStep && (
             <StepBar
