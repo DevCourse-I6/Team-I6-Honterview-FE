0f1e1e99b4b69d350001f177bc86e586a9bc5b2f
지인혁 <dhgg321@naver.com>
2024-03-21 10:11:33 +0900

    Fix: 빌드 오류 수정

--- src/components/Header/components/hamburgerMenu/components/mobileLogOutButton/index.tsx
+++ src/components/Header/components/hamburgerMenu/components/mobileLogOutButton/index.tsx
@@ -1,14 +1,28 @@
-import { logOut } from '@/libs/actions/auth';
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { logOut } from '@/libs/services/auth';
 
 const MobileLogoutButton = () => {
+  const router = useRouter();
+
+  const handleLogOut = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    await logOut();
+    router.refresh();
+  };
+
   return (
-    <button
-      type="submit"
-      formAction={logOut}
-      className="w-full px-10 py-4 text-center hover:bg-background-20"
-    >
-      로그아웃
-    </button>
+    <form>
+      <button
+        type="submit"
+        onClick={handleLogOut}
+        className="w-full px-10 py-4 text-center hover:bg-background-20"
+      >
+        로그아웃
+      </button>
+    </form>
   );
 };
 
