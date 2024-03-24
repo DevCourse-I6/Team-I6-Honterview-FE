e9114ee2bc6dfcbdfd6f957316c4da35e1a18f6a
지인혁 <87266785+wldlsgur@users.noreply.github.com>
2024-03-24 21:30:44 +0900

    Fix: 로그인 렌더링 관련 버그 임시 수정 (#107)

--- src/components/Header/components/linkButtons/index.tsx
+++ src/components/Header/components/linkButtons/index.tsx
@@ -1,30 +1,15 @@
-import { revalidateTag } from 'next/cache';
 import Link from 'next/link';
 
 import DividerVertical from '@/components/dividerVertical';
-import ReissueAccessTokenComponent from '@/components/reissueAccessTokenComponent';
 import { getUserAuth } from '@/libs/services/auth';
 
 import LogoutButton from './components/logoutButton';
 
 const LinkButtons = async () => {
-  const { status, data: auth } = await getUserAuth();
+  const { data: auth } = await getUserAuth();
 
   return (
     <div className="hidden min-w-[10%] items-center justify-end gap-6 md:flex">
-      <ReissueAccessTokenComponent<Promise<void>, Promise<void>>
-        status={status}
-        callback={async () => {
-          'use server';
-
-          revalidateTag('userAuth');
-        }}
-        onFail={async () => {
-          'use server';
-
-          null;
-        }}
-      />
       {auth ? (
         <div className="flex items-center justify-end gap-6">
           <Link href="/mypage">마이페이지</Link>
